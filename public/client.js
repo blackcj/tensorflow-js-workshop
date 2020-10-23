13f3b0b19a8a0ba99fe572a875f176ee0db5e0e7
Christopher Black <chris@blacktopinteractive.com>
2020-10-23 13:18:14 -0500

    Added styling. Updated JavaScript to use jQuery.

--- public/client.js
+++ public/client.js
@@ -5,12 +5,11 @@
 const URL = './demo-image-model/';
 
 let model, webcam, labelContainer, maxPredictions;
+const flip = true; // whether to flip the webcam
 
-// Load the image model and setup the webcam
-async function init() {
+$(document).ready(async function() {
     const modelURL = URL + 'model.json';
     const metadataURL = URL + 'metadata.json';
-
     // load the model and metadata
     // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
     // or files from your local hard drive
@@ -18,18 +17,25 @@ async function init() {
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
+    $('#start-button').on('click', function() {
+        $('#start-button').hide();
+        init();
+    });
+});
+
+// Load the image model and setup the webcam
+async function init() {
     // Convenience function to setup a webcam
-    const flip = true; // whether to flip the webcam
-    webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+    webcam = new tmImage.Webcam(400, 300, flip); // width, height, flip
     await webcam.setup(); // request access to the webcam
     await webcam.play();
     window.requestAnimationFrame(loop);
 
     // append elements to the DOM
-    document.getElementById('webcam-container').appendChild(webcam.canvas);
-    labelContainer = document.getElementById('label-container');
+    $('#webcam-container').append(webcam.canvas);
+    labelContainer = $('#label-container');
     for (let i = 0; i < maxPredictions; i++) { // and class labels
-        labelContainer.appendChild(document.createElement('div'));
+        $('#label-container').append('<div></div>');
     }
 }
 
@@ -44,8 +50,7 @@ async function predict() {
     // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].innerHTML = classPrediction;
+        const classPrediction = prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+        $('#label-container').children().eq(i).html(classPrediction)
     }
 }
